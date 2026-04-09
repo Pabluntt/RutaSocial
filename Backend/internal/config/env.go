2530c413e86e37a01e01d446f4fc5538e686e14b
Garcia-IGC <ignacio.garcia02@alumnos.ucn.cl>
2026-04-09 12:35:00 -0400

    Configure Railway deployment for Backend and FrontendWeb

--- Backend/internal/config/env.go
+++ Backend/internal/config/env.go
@@ -3,6 +3,8 @@ package config
 
 import (
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 // JwtSecret es la clave secreta utilizada para firmar y verificar tokens JWT.
@@ -11,6 +13,8 @@ var JwtSecret []byte
 // LoadEnv carga las variables de entorno necesarias para la aplicación.
 // En este caso, carga la clave secreta para JWT desde la variable de entorno
 func LoadEnv() error {
+	// Cargar el archivo .env
+	_ = godotenv.Load()
 
 	JwtSecret = []byte(os.Getenv("JWT_SECRET"))
 
